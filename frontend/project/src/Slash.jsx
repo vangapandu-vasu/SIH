9d3efbafb6d89e03874e0a558f40f089c66b7c49
vangapandu-vasu <naiduvasu72@gmail.com>
2025-09-06 07:47:24 +0530

    addition of code in slash

--- frontend/project/src/Slash.jsx
+++ frontend/project/src/Slash.jsx
@@ -1,7 +1,33 @@
 function Slash(){
     return(
     <>
-
+        <header className="header">
+            <div className="header-div">
+                <h1>logo</h1>
+            </div>
+        </header>
+        <main className="main">
+            <div className="main-div" id="heading">
+                <h2>
+                    Choose Who You Are
+                </h2>
+            </div>
+            <div className="main-div" id="user">
+                <span>
+                    user
+                </span>
+            </div>
+            <div className="main-div" id="mentor">
+                <span>
+                    mentor
+                </span>
+            </div>
+        </main>
+        <footer className="footer">
+            <div className="footer-div">
+                will see
+            </div>
+        </footer>
     </>
     )
 }
