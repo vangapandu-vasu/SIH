472c1e0ab0b0d48b1a71f5f188bfd80f980dec4a
vangapandu-vasu <naiduvasu72@gmail.com>
2025-09-06 17:53:12 +0530

    addition of code in user and mentor

--- frontend/project/src/user.jsx
+++ frontend/project/src/user.jsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
+
 function User(){
+
+    const [email,setEmail]= useState("");
+    const [password, setPassword]=useState("");
+    const [errmsg,setErrmsg] = useState("");
+
+    let handlelogin=(e)=>{
+        e.prventDefault();
+        // database pass
+    }
     return(
     <>
-        <h1>
-            Userrrrr
-        </h1>
+        <div className="formone">
+            <div className="loginone" id="logo">
+                <h5>LOGIN</h5>
+            </div>
+            <form onSubmit={(e)=>handlelogin(e)}>
+                <div className="loginone" id="namee">
+                    <input className="email"
+                        placeholder="enter your email"
+                        type="text"
+                        onChange={(e)=>setEmail(e.target.value)}
+                        required
+                    ></input>
+                </div>
+                <div className="loginone" id="password">
+                    <input className="password"
+                        placeholder="enter your password"
+                        type="password"
+                        onChange={(e)=>setPassword(e.target.value)}
+                        required
+                    ></input>   
+                </div>
+                {errmsg && <p className="error-msg">{errmsg}</p>}
+                <div className="loginone" id="buttonn">
+                    <button  type="submit">login</button>
+                </div>
+            </form>
+        </div>
     </>
     );
 }
